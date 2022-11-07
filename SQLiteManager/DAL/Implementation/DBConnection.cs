f21677916e3b896cc6163d8f4dc6f41df0683018
UrekMazin0 <rostov.repin99@gmail.com>
2022-11-07 22:13:58 +0300

    refactor: dbconnection debug and release mode

--- SQLiteManager/DAL/Implementation/DBConnection.cs
+++ SQLiteManager/DAL/Implementation/DBConnection.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SQLite;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -18,11 +19,16 @@ namespace SQLiteManager.DAL.Implementation
 			// но другого я ничего не придумал
 			get 
 			{
+#if DEBUG
 				if (dbFile == null)
 					dbFile = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName +
 							 Path.DirectorySeparatorChar + "database" + Path.DirectorySeparatorChar + "ntsDB.db";
-
 				return dbFile;
+#else
+				if (dbFile == null)
+					dbFile = "database" + Path.DirectorySeparatorChar + "ntsDB.db";
+				return dbFile;
+#endif
 			}
 		}
 
